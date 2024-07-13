a80363b1e5062aa612d0eaadd3a18f38578755f5
Tanvir Meah <37561141+tanvirmeah@users.noreply.github.com>
2024-07-14 00:14:23 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,29 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import MobileNav from "./MobileNav";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
-          src="./icons/logo.svg"
+          src="/icons/logo.svg"
           width={32}
           height={32}
-          alt="Yoom logo "
+          alt="meetly logo"
           className="max-sm:size-10"
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          Yoom
+          meetly
         </p>
       </Link>
       <div className="flex-between gap-5">
         <SignedIn>
-          <UserButton />
+          <UserButton afterSignOutUrl="/sign-in" />
         </SignedIn>
-        
+
         <MobileNav />
       </div>
     </nav>
